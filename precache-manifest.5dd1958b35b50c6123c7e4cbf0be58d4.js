11dd9a523c3369fe1ca06e3ce94f8738399c52ab
zhengyi <zhengyi@is-keen.com>
2019-04-16 14:49:38 +0800

    update

---
+++ precache-manifest.5dd1958b35b50c6123c7e4cbf0be58d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dcb8f05aa7e38cc61d8",
+    "url": "./static/css/main.c02f05f1.chunk.css"
+  },
+  {
+    "revision": "1dcb8f05aa7e38cc61d8",
+    "url": "./static/js/main.5dfd5713.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "71083d2a398964461f5c",
+    "url": "./static/js/2.c5dc7e67.chunk.js"
+  },
+  {
+    "revision": "abc9ed79281340024cff5f15716d0b8e",
+    "url": "./index.html"
+  }
+];
