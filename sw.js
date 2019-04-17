d2794f686aa764ba198aa68604a4af4b3a4275d4
zhengyi <zhengyi@is-keen.com>
2019-04-17 15:23:49 +0800

    uf

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
   var payload = event.data ? event.data.text() : 'no payload';
-  var title = 'Progressive Times';
+  var title = '消息标题adfdsf';
   event.waitUntil(
     // 使用提供的信息来显示 Web 推送通知
     self.registration.showNotification(title, {
